8e82e3da8f2f16b8ee1452b78105e070f9399520
Akira Takahashi <rike422@gmail.com>
2016-10-27 13:51:14 +0900

    Separate

--- src/client/editor-client.js
+++ src/client/editor-client.js
@@ -1,5 +1,5 @@
-import { hsl2hex, hueFromName } from 'common/colors'
 import Client from 'client/client'
+import OtherClient from 'client/other-client'
 import Selection from 'client/selection'
 import UndoManager from 'client/undo-manager'
 import TextOperation from 'ot/text-operation'
@@ -24,92 +24,6 @@ class SelfMeta {
       this.selectionBefore.transform(operation),
       this.selectionAfter.transform(operation)
     )
-  }
-}
-
-class OtherMeta {
-  constructor (clientId, selection) {
-    this.clientId = clientId
-    this.selection = selection
-  }
-
-  transform (operation) {
-    return new OtherMeta(
-      this.clientId,
-      this.selection && this.selection.transform(operation)
-    )
-  }
-}
-
-OtherMeta.fromJSON = obj => new OtherMeta(
-  obj.clientId,
-  obj.selection && Selection.fromJSON(obj.selection)
-)
-
-class OtherClient {
-  constructor (id, listEl, editorAdapter, name, selection) {
-    this.id = id
-    this.listEl = listEl
-    this.editorAdapter = editorAdapter
-    this.name = name
-
-    this.li = document.createElement('li')
-    if (name) {
-      this.li.textContent = name
-      this.listEl.appendChild(this.li)
-    }
-
-    this.setColor(name ? hueFromName(name) : Math.random())
-    if (selection) {
-      this.updateSelection(selection)
-    }
-  }
-
-  setColor (hue) {
-    this.hue = hue
-    this.color = hsl2hex(hue, 0.75, 0.5)
-    this.lightColor = hsl2hex(hue, 0.5, 0.9)
-    if (this.li) {
-      this.li.style.color = this.color
-    }
-  }
-
-  setName (name) {
-    if (this.name === name) {
-      return
-    }
-    this.name = name
-
-    this.li.textContent = name
-    if (!this.li.parentNode) {
-      this.listEl.appendChild(this.li)
-    }
-
-    this.setColor(hueFromName(name))
-  }
-
-  updateSelection (selection) {
-    this.removeSelection()
-    this.selection = selection
-    this.mark = this.editorAdapter.setOtherSelection(
-      selection,
-      selection.position === selection.selectionEnd ? this.color : this.lightColor,
-      this.id
-    )
-  }
-
-  remove () {
-    if (this.li) {
-      removeElement(this.li)
-    }
-    this.removeSelection()
-  }
-
-  removeSelection () {
-    if (this.mark) {
-      this.mark.clear()
-      this.mark = null
-    }
   }
 }
 
@@ -322,9 +236,3 @@ export default class EditorClient extends Client {
   }
 }
 
-// Remove an element from the DOM.
-function removeElement (el) {
-  if (el.parentNode) {
-    el.parentNode.removeChild(el)
-  }
-}
